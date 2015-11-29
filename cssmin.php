f66a86b898fb9e2fae1c3ee34d9c2cd8d6688b32
frank goossens <futtta@gmail.com>
2015-11-29 07:34:35 +0100

    potential fix for issue 26
    cfr. https://github.com/tubalmartin/YUI-CSS-compressor-PHP-port/issues/26

--- cssmin.php
+++ cssmin.php
@@ -264,8 +264,11 @@ class CSSmin
         // Normalize all whitespace strings to single spaces. Easier to work with that way.
         $css = preg_replace('/\s+/', ' ', $css);
 
-		// Fix IE7 issue on matrix filters which browser accept whitespaces between Matrix parameters
-		$css = preg_replace_callback('/\s*filter\:\s*progid:DXImageTransform\.Microsoft\.Matrix\(([^\)]+)\)/', array($this, 'preserve_old_IE_specific_matrix_definition'), $css);
+	// preserve flex, keeping percentage even if 0
+	$css = preg_replace_callback('/flex\s?:\s?([0-9 %]*)/i',array($this, 'replace_flex'),$css);
+
+	// Fix IE7 issue on matrix filters which browser accept whitespaces between Matrix parameters
+	$css = preg_replace_callback('/\s*filter\:\s*progid:DXImageTransform\.Microsoft\.Matrix\(([^\)]+)\)/', array($this, 'preserve_old_IE_specific_matrix_definition'), $css);
 
         // Shorten & preserve calculations calc(...) since spaces are important
         $css = preg_replace_callback('/calc(\(((?:[^\(\)]+|(?1))*)\))/i', array($this, 'replace_calc'), $css);
@@ -594,10 +597,16 @@ class CSSmin
         return 'calc('. self::TOKEN . (count($this->preserved_tokens) - 1) . '___' . ')';
     }
 
-	private function preserve_old_IE_specific_matrix_definition($matches)
-	{
-		$this->preserved_tokens[] = $matches[1];
-		return 'filter:progid:DXImageTransform.Microsoft.Matrix(' . self::TOKEN . (count($this->preserved_tokens) - 1) . '___' . ')';
+    private function replace_flex($matches)
+    {
+        $this->preserved_tokens[] = trim($matches[1]);
+        return 'flex:'.self::TOKEN . (count($this->preserved_tokens) - 1) . '___';
+    }
+    
+    private function preserve_old_IE_specific_matrix_definition($matches)
+    {
+	$this->preserved_tokens[] = $matches[1];
+	return 'filter:progid:DXImageTransform.Microsoft.Matrix(' . self::TOKEN . (count($this->preserved_tokens) - 1) . '___' . ')';
     }
 
 	private function replace_keyframe_zero($matches)
