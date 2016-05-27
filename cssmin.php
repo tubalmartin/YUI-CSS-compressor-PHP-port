62b3674cc3920d667534b7741dace0317fdf58e1
frank goossens <futtta@gmail.com>
2016-05-27 11:33:47 +0200

    Revert "potential fix for issue 22"
    This reverts commit 5d226738b26e69ecff9a89d5898396d1a1dbff44.

--- cssmin.php
+++ cssmin.php
@@ -590,7 +590,7 @@ class CSSmin
 
     private function replace_calc($matches)
     {
-        $this->preserved_tokens[] = trim($matches[2]);
+        $this->preserved_tokens[] = trim(preg_replace('/\s*([\*\/\(\),])\s*/', '$1', $matches[2]));
         return 'calc('. self::TOKEN . (count($this->preserved_tokens) - 1) . '___' . ')';
     }
 
